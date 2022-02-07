bdea619420ff9cfad494f02eca15e456242fecdd
Frederik J. Simons <fjsimons@gmail.com>
2022-02-07 11:47:40 -0500

    Equalizes observation epochs between MAT files

--- mat2mod.m
+++ mat2mod.m
@@ -20,35 +20,22 @@ function varargout=mat2mod(files)
 % 
 % Originally written by tschuh-at-princeton.edu, 11/12/2021
 % Last modified by tschuh-at-princeton.edu, 11/15/2021
-% Last modified by fjsimons-at-alum.mit.edu, 02/03/2022
+% Last modified by fjsimons-at-alum.mit.edu, 02/07/2022
 
 % Non-array variables to exclude from the tabling procedure
-drem={'xyzunit','lonlatunit','utmunit','heightunit','satlabels'};
+drem={'xyzunit','latlonunit','utmunit','heightunit','satlabels','utmzone'};
+drem={'xyzunit','lonlatunit','utmunit','heightunit','satlabels','utmzone'};
 
 for i=1:length(files)
     load(files{i});
-    % Only get the wanted variables, turn them into a timetable
     % Use (RE)TIME(TABLE) to fill in time skips/data gaps with NaNs
-    tt=retime(table2timetable(struct2table(rmfield(d,drem))),'secondly','fillwithmissing');
-    % Reassign to the old STRUCT
-    d.t=tt.t;
-    % These are the variables we kept
-    fnd=fieldnames(rmfield(d,drem));
-    % And they appear shifted by one in the time table
-    fnt=fieldnames(tt);
-    % So now you put them back in the right place as a struct
-    for j=2:length(fnd)
-      d.(fnd{j})=tt.(fnt{j-1});
-    end    
+    [d,fnd]=retimes(d,drem,{'secondly','fillwithmissing'});
     % Assemble for later use
     dmat(i) = d;
 end
 
-% About to change this to: all files start and end with a datum, but
-% in-between their values, there could be skips, which now have been
-% fixed by setting them to NaNs. The longest common overlapping segment
-% goes from the latest start to the earliest end of any file.
-% Assuming you know there IS a field t.
+% The longest common overlapping segment goes from the latest start to the
+% earliest end of any file. Assuming you know there IS a field t...
 for i=1:length(files)
    B(i)=dmat(i).t(1);
    E(i)=dmat(i).t(end);

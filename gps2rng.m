9a4c8414525be0c1af4bf490040849afe42aa153
Frederik J. Simons <fjsimons@gmail.com>
2022-02-08 10:39:59 -0500

    Calculates distances between PPP points and a target

--- gps2rng.m
+++ gps2rng.m
@@ -22,29 +22,28 @@ function varargout=gps2rng(files,meth,xyz,v)
 %
 % gps2rng({'0001-05340.mat','0002-05340.mat','0003-05340.mat','0004-05340.mat'})
 % gps2rng({'Unit1-camp.mat','Unit2-camp.mat','Unit3-camp.mat','Unit4-camp.mat'})
-% gps2rng({'Unit2.camp.mat'})
+% gps2rng({'Unit2-camp.mat'})
 %
 % Originally written by tschuh-at-princeton.edu, 11/24/2021
 % Last modified by tschuh-at-princeton.edu 02/03/2022
 % Last modified by tschuh-at-princeton.edu 02/07/2022
 
-% inspired by file 05040 first xyz positions, but ~5 km below surface
-% eventually will need to be a changing value until it reaches seafloor
-% after that will not be known and will be deduced from timestamps sent back
-defval('xyz',[1.979e6 -5.074e6 3.30385e6])
-
+% how the possibly multiple receivers will be jointly considered
 defval('meth','ave')
 % constant sound speed profile for now [m/s]
 defval('v',1500)
 
-% set beacon location on seafloor
-dogx=xyz(1);
-dogy=xyz(2);
-dogz=xyz(3);
-
 % Get extension from first filename
 [~,fname,~]=fileparts(files{1});
 filex=suf(fname,'-');
+% If more than one input, list them all
+if length(files)>1
+  for index=1:length(files)
+    [~,fname,~]=fileparts(files{index});
+    nr(index)=indeks(pref(fname,'-'),'end');
+  end
+  fname=sprintf('Unit%s-%s',nr(1:end),suf(pref(files{1},'.'),'-'));
+end
 
 % combine all datasets into 1 with no plotting
 [d,tmax]=mat2mod(files);
@@ -54,12 +53,41 @@ ntix=7;
 ttix=[1 round([1:ntix-1]*length(d(1).t)/(ntix-1))];
 tixl=datestr(d(1).t(ttix),'HH:MM:SS');
 
-% Averaging method, or keeping them invidual, or taking only one,
-switch meth
-  case 'ave'
-   dxyz=squeeze(nanmean(reshape(cat(1,d(:).xyz),size(d(1).xyz,1),length(d),3),2));
+% Averaging method, or keeping them invidual, or taking only one...
+
+if length(files)>1
+  switch meth
+   case 'ave'
+    dxyz=squeeze(nanmean(reshape(cat(1,d(:).xyz),size(d(1).xyz,1),length(d),3),2));
+  end
+else
+  % Save yourself the trouble
+  dxyz=d.xyz;
 end
 
+% Determine a starting point from the middle section
+imeth='down'; 
+
+switch imeth
+ case 'down'
+   % Put in water depth read off the GPSTRAJECT map
+   depth=5225; 
+   % Just about
+   % lonlat=[291.3001853867659   31.4661752724632];
+   % z=gebco(lonlat(1)-360,lonlat(2));
+   % Handpicked by GINPUT on a plot of dxyz...
+   [dogx,dogy,dogz]=gps2dep([1.977967 -5.073198 3.3101016]*1e6,depth);
+ case 'guess'
+  % Likely water depth from PrincetonSeafloorGeodesy-SURVEY3.pdf ORIGIN
+  defval('depth',gebco(68+42/60,-(31+27/60)));
+  % Rather put in a better water depth, so all units return uniform
+  % results, and read off the GPSTRAJECT map
+  depth=5225;
+  msex=[1 3]/4;
+  midsex=[round(msex(1)*size(dxyz,1)):round(msex(2)*size(dxyz,1))];
+  [dogx,dogy,dogz]=gps2guess(dxyz(midsex,:),depth);
+end
+   
 % calculate slant range between ship and beacon for each second in meters
 sr=sqrt((dxyz(:,1)-dogx).^2+(dxyz(:,2)-dogy).^2+(dxyz(:,3)-dogz).^2);
 
@@ -84,6 +112,8 @@ if nargout==0
   ah(1).XLim=xel+[-1 1]*range(xel)/20;
   ah(1).YLim=yel+[-1 1]*range(yel)/20;
   ylabel('slant range [m]')
+  % Be sure to mark the minimum
+  
   % Here you'll see that the total time represented could be wider than any
   % single one, since you've done MAT2MOD
   tl(1)=title(sprintf('Raw Slant Range Measurements: %s-%s',...
@@ -95,7 +125,9 @@ if nargout==0
   xticklabels(tixl)
 
   % Arbitrarily absolute, ok for the moment
-  tt(1)=text(tmax(1),15000,sprintf('%i%s NaN',...
+  yl=ylim;
+  topy=yl(2)-[yl(2)-yl(1)]/20;
+  tt(1)=text(tmax(1),topy,sprintf('%i%s NaN',...
 				   round(sum(isnan(sr))/length(sr)*100),'%'),...
 	     'VerticalAlignment','top');
 
@@ -104,7 +136,7 @@ if nargout==0
     stran=sprintf('%s%s\n',stran,pref(files{index},'-'));
   end
   stran=stran(1:end-1);
-  tt(2)=text(tmax(2),15000,stran,'HorizontalAlignment','right',...
+  tt(2)=text(tmax(2),topy,stran,'HorizontalAlignment','right',...
 	     'VerticalAlignment','top');
   set(tt(:),'FontSize',8);
 

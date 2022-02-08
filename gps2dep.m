e0d98a7b3fe29630f068bcd03bcd12228ff75a25
Frederik J. Simons <fjsimons@gmail.com>
2022-02-08 10:39:57 -0500

    Finds an x,y,z subsurface point at a depth below an x,y,z surface point

---
+++ gps2dep.m
@@ -0,0 +1,25 @@
+function [x0,y0,z0]=gps2depth(dxyz,depth)
+% [x0,y0,z0]=GPS2DEPTH(dxyz,depth)
+%
+% Given a point set of GPS coordinates and a depth, finds a point at depth
+% below that point.
+%
+% INPUT:
+%
+% dxyz         The GPS points
+% depth        The nominal depth
+%
+% OUTPUT:
+%
+% x0,y0,z0     A sensible subsurface location guess
+%
+% Last modified by fjsimons-at-alum.mit.edu, 02/07/2022
+
+% Likely water depth from PrincetonSeafloorGeodesy-SURVEY3.pdf ORIGIN
+defval('depth',gebco(68+42/60,-(31+27/60)));
+
+% The GPS points in spherical coordinates
+[AZ,EL,r]=cart2sph(dxyz(:,1),dxyz(:,2),dxyz(:,3));
+% All possible xyz points for every surface point except at depth
+[x0,y0,z0]=sph2cart(AZ,EL,r-abs(depth));
+

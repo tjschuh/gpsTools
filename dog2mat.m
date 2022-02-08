207efecf33c9aac0de1371466d4d480d5b62b28c
Frederik J. Simons <fjsimons@gmail.com>
2022-02-08 10:39:57 -0500

    Saves DOG *.txt files in a directory into a *.mat file

--- dog2mat.m
+++ dog2mat.m
@@ -26,7 +26,26 @@ for index=1:length(files)
   tags=[tags ; d];
 end
 
+% Save to MAT
+
+% Make a plot if there is no output? Watch out for implicit vector sums
+%plot([tags(:,1)-tags(1,1)]*1e9+tags(:,2)-[1:size(tags(:,1),1)]')
+%plot(tags(:,1)*1e9+tags(:,2))
+
 % Optional output
 varns={tags};
 varargout=varns(1:nargout);
 
+% 1 and 3 have the most points
+load DOG1-camp
+[sr]=gps2rng({'Unit3-camp.mat'});
+inta=floor(sr/1500);
+plot(sr/1600)
+hold on
+ofs=0
+for ofs=000:3000
+  p=plot(tags(:,2)/1e9+inta([1:size(tags,1)]+ofs),'k');
+  title(num2str(ofs))                              
+  pause
+  delete(p)
+end

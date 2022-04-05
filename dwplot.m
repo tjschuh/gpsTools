d0af69f50c9d3bd81cce81f96d6921727608f366
Terance Schuh <tschuh@princeton.edu>
2022-04-05 18:48:05 -0400

    plot synthetic results

--- dwplot.m
+++ dwplot.m
@@ -1,14 +1,24 @@
 function dwplot(data,thresh)
-% ADD HEADER
+% DWPLOT(data,thresh)
+%
+% description
+%
+% INPUT:
+%
+% data
+% thresh
+%
+% Originally written by tschuh-at-princeton.edu, 04/05/2022
 
-defval('thresh',0.5)
+% p-value threshold
+defval('thresh',0.05)
 
 sz = 50;
 
 % plot ellipsoid
 figure(1)
 for i=1:length(data)          
-    if data(i,4) > 2-thresh & data(i,4) < 2+thresh
+    if data(i,4) > thresh
         scatter3(data(i,1),data(i,2),data(i,3),sz,'filled','k')
         hold on
     end
@@ -19,10 +29,11 @@ zlabel('z')
 title(sprintf('Locations where Durbin-Watson test result = 2 +/- %g',thresh))
 grid on
 longticks
+hold off
 
 % plot all values cube
 figure(2)
-scatter3(data(:,1),data(:,2),data(:,3),sz,data(:,4),'filled')
+scatter3(data(:,1),data(:,2),data(:,3),sz,data(:,6),'filled')
 a = colorbar;
 a.Label.String = 'p-value';
 a.FontSize = 11;
@@ -36,7 +47,7 @@ longticks
 slice = data(find(data(:,3) == 0),:);
 
 figure(3)
-scatter(slice(:,1),slice(:,2),sz,slice(:,4),'filled')
+scatter(slice(:,1),slice(:,2),sz,slice(:,6),'filled')
 a = colorbar;
 a.Label.String = 'p-value';
 a.FontSize = 11;

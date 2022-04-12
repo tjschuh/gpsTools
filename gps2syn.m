f9803754fc6f693c99bf499076c1baf94847d0b8
Terance Schuh <tschuh@princeton.edu>
2022-04-12 15:44:54 -0400

    simple synthetic experiment

--- gps2syn.m
+++ gps2syn.m
@@ -31,7 +31,7 @@ function varargout = gps2syn(d,tmax,xyz,xyzn,v,vn,plt)
 % end
 %
 % Originally written by tschuh-at-princeton.edu, 02/23/2022
-% Last modified by tschuh-at-princeton.edu, 04/11/2022
+% Last modified by tschuh-at-princeton.edu, 04/12/2022
 
 % default plotting is off
 defval('plt',0)
@@ -66,6 +66,8 @@ vg = v0 + vn;
 
 % add uncertainty to ship locations (+/- 0.02 m)
 % draw a number from a normal distribution from -2 to 2
+% this is incorrect --> need to change std 2/3 --> 2
+% also change z to have larger std b/c that's what we observed
 d.x0 = d.x; d.y0 = d.y; d.z0 = d.z;
 for i = 1:length(d.x0)
     d.x(i) = d.x0(i) + (normrnd(0,2/3)*1e-2);

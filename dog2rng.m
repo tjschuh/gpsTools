9ae1e283799a3e0d4827de3a6e037c38b7432bf3
Terance Schuh <tschuh@princeton.edu>
2022-07-07 16:31:13 -0400

    Unwraps relative arrival times from the DOG *.mat files

--- dog2rng.m
+++ dog2rng.m
@@ -1,88 +1,132 @@
-function dog2rng(fname)
-% DOG2RNG(fname)
+function dog2rng(fname,num)
+% DOG2RNG(fname,num)
 %
 % Works on a file made by DOG2MAT and turns it into a range measurement
+%
+% INPUT:
+%
+% fname         file made by DOG2MAT with time tags
+% num           dog number corresponding to file (temporary input)
 %
 % EXAMPLE:
 %
 % dog2rng({'DOG1-camp.mat'})
+%
+% Originally written by fjsimons-at-princeton.edu, 02/07/2022
+% Last modified by tschuh-at-princeton.edu, 07/07/2022
 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Unwrap disregards NaN's but maybe it shouldn't
+% Could INTERPOLATE over the NaNs for the unwrapping, then undo the interpolation
 
-% % Unwrap disregards NaN's but maybe it shouldn't
-% % Could INTERPOLATE over the NaNs for the unwrapping, then undo the interpolation
-% % Load the ranges with the "dead pixels" nan-ified - singles or jointly
+% Load the ranges with the "dead pixels" nan-ified - singles or jointly
+% Note this is about right with respect to DOG1
+st=gps2rng({'Unit1-camp.mat','Unit2-camp.mat','Unit3-camp.mat','Unit4-camp.mat'});
 
+figure(1)
+clf
 
-% % Note this is about right with respect to DOG1
-% st=gps2rng({'Unit1-camp.mat','Unit2-camp.mat','Unit3-camp.mat','Unit4-camp.mat'});
+% plot GNSS slant times from ship positions
+plot(st,'b'); hold on
 
-% subplot(211)
-% plot(st,'b'); hold on
-% % Load the dog tags
-% load DOG1-camp
-% ofx1=5823;
-% % This next one is dependent on n below
-% ofx2=4550; n=2;
-% ofx2=4800; n=1;
-% % Another range for a different ofy
-% ofxy1=73606;
-% ofy1=3.1;
-% ofy2=-0.925;
+% Load the dog tags
+load(char(fname))
 
-% % First, you eyeball an offset and cut it off
-% tags=tags(ofx1:end,1:2);
-% % And now you correct where the big negative jumps in the dependent
-% % variable are, they need an extra second, and where they are big
-% % positive they stop needing that, so that's unwrapping; the result is in seconds
-% tags(:,2)=unwrap(tags(:,2)/1e9*2*pi)/2/pi;
+if num == 1
+    ofx1=5823;
+    % This next one is dependent on n below
+    ofx2=4800; n=1;
+    % Another range for a different ofy
+    ofxy1=73606;
+    ofy1=3.1;
+    ofy2=-0.925;
 
-% % Then you plot and look
-% p(1)=plot(tags(:,2)+ofy1,'g.','MarkerSize',3);
+    % First, you eyeball an offset and cut it off
+    tags=tags(ofx1:end,1:2);
+    % And now you correct where the big negative jumps in the dependent
+    % variable are, they need an extra second, and where they are big
+    % positive they stop needing that, so that's unwrapping; the result is in seconds
+    tags(:,2)=unwrap(tags(:,2)/1e9*2*pi)/2/pi;
 
-% % Now need to stick in extra NaNs
+    p(1)=plot(tags(:,2)+ofy1,'g.','MarkerSize',3);
+    
+    % Now need to stick in extra NaNs
 
-% % Shots are every seconds so difference between arrival times cannot be
-% % much more than that, since the boat doesn't go that fast, and the
-% % velocity of the water doesn't change that much. So whenever the second
-% % skips by more than n, there must be a missed detection
-% ntags=retimei(tags,n);
+    % Shots are every seconds so difference between arrival times cannot be
+    % much more than that, since the boat doesn't go that fast, and the
+    % velocity of the water doesn't change that much. So whenever the second
+    % skips by more than n, there must be a missed detection
+    ntags=retimei(tags,n);
 
-% % Then, you eyeball a speed correction
-% ntags(1:ofxy1,2)=ntags(1:ofxy1,2)+ofy1;
-% ntags(ofxy1+1:end,2)=ntags(ofxy1+1:end,2)+ofy2;
+    % Then, you eyeball a speed correction
+    ntags(1:ofxy1,2)=ntags(1:ofxy1,2)+ofy1;
+    ntags(ofxy1+1:end,2)=ntags(ofxy1+1:end,2)+ofy2;
 
-% % Eyeball a new offset and cut that off
-% ntags=ntags(ofx2:end,1:2);
+    % Eyeball a new offset and cut that off
+    ntags=ntags(ofx2:end,1:2);
 
-% % And replot those new tags
-% p(2)=plot(ntags(:,2),'r.','MarkerSize',3);
+    % And replot those new tags
+    p(2)=plot(ntags(:,2),'r.','MarkerSize',3);
+    keyboard
+    delete(p(1))
+    hold off
+elseif num == 2
+    tags(:,2)=unwrap(tags(:,2)/1e9*2*pi)/2/pi;
+    p(1)=plot(tags(:,2),'g.','MarkerSize',3);
 
-% % Then get rid of the old
-% delete(p(1))
-% hold off
+    ntags=retimei(tags,1);
 
-% Nh=24;
-% xlim([0 3600*Nh])
-% ylim([3 10])
-% longticks(gca,2)
-% grid on
-% xlabel('time [h]')
-% nh=3;
-% xticks(0:nh*3600:size(ntags,1))
-% xticklabels(0:nh:floor(size(ntags,1)/3600))
-% ylabel('slant range time [s]')
+    p(2)=plot(ntags(:,2),'r.','MarkerSize',3);
+    keyboard
+elseif num == 3
+    ofx1=1; ofy1 = 1;
+    ofx2=12000; ofy2=-3;
+    n=1; ofxy1=80000;
+    
+    % unwrap first
+    tags(:,2)=unwrap(tags(:,2)/1e9*2*pi)/2/pi;
 
-% hold on
-% pl(1)=plot(-1,-1,'b-');
-% pl(2)=plot(-1,-1,'r-');
-% hold off
-% legs=legend(pl,'GNSS','Acoustic','FontSize',6,'Location','northeast');
+    % then cut off beginning by eyeballing
+    tags=tags(ofx1:end,1:2)+ofy1;
 
-% moveh(legs,0.101)
-% movev(legs,0.01)
+    p(1)=plot(tags(:,2),'g.','MarkerSize',3);
 
-% figdisp('dog2mat',[],[],2)
+    % then retime
+    ntags=retimei(tags,n);
+
+    ntags(1:ofxy1,2)=ntags(1:ofxy1,2);
+    ntags(ofxy1+1:end,2)=ntags(ofxy1+1:end,2)+ofy2;
+    
+    ntags=ntags(ofx2:end,1:2);
+
+    p(2)=plot(ntags(:,2),'r.','MarkerSize',3);
+    keyboard
+    delete(p(1))
+    hold off
+    keyboard
+elseif num == 4
+    % load in DOG4
+end
+    
+% cosmetics
+Nh=24;
+xlim([0 3600*Nh])
+ylim([0.85*min(ntags(:,2)) 1.05*max(ntags(:,2))])
+longticks(gca,2)
+grid on
+xlabel('time [h]')
+nh=3;
+xticks(0:nh*3600:size(ntags,1))
+xticklabels(0:nh:floor(size(ntags,1)/3600))
+ylabel('slant range time [s]')
+% plot legend
+hold on
+pl(1)=plot(-1,-1,'b-');
+pl(2)=plot(-1,-1,'r-');
+hold off
+legs=legend(pl,'GNSS','Acoustic','FontSize',6,'Location','northeast');
+keyboard
+% print pdf
+figdisp('dog2mat2',[],'',2,[],'epstopdf')    
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 

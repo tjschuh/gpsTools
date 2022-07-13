7b1d61cda9703bddf950b57e4f08ebedfa06df95
Terance Schuh <tschuh@princeton.edu>
2022-07-13 16:39:46 -0400

    Unwraps relative arrival times from the DOG *.mat files

--- dog2rng.m
+++ dog2rng.m
@@ -10,17 +10,31 @@ function dog2rng(fname,num)
 %
 % EXAMPLE:
 %
-% dog2rng({'DOG1-camp.mat'})
+% dog2rng({'DOG1-camp.mat'},1)
 %
 % Originally written by fjsimons-at-princeton.edu, 02/07/2022
-% Last modified by tschuh-at-princeton.edu, 07/07/2022
+% Last modified by tschuh-at-princeton.edu, 07/13/2022
+
+% make this much cleaner and lose duplicate code
 
 % Unwrap disregards NaN's but maybe it shouldn't
 % Could INTERPOLATE over the NaNs for the unwrapping, then undo the interpolation
 
 % Load the ranges with the "dead pixels" nan-ified - singles or jointly
-% Note this is about right with respect to DOG1
-st=gps2rng({'Unit1-camp.mat','Unit2-camp.mat','Unit3-camp.mat','Unit4-camp.mat'});
+if num == 1
+    % Note this is about right with respect to DOG1
+    xyz=[1.977967 -5.073198 3.3101016]*1e6;
+elseif num == 2
+    % DOG2 approx sea-surface xyz position (this is about right)
+    xyz=[1.9765409 -5.074798706 3.308558817]*1e6;
+elseif num == 3
+    % DOG3 approx position (this is about right)
+    xyz=[1.980083296 -5.073417583 3.308558817]*1e6;
+elseif num == 4
+    % DOG4 approx position (this is about right)
+    xyz=[1.978732384 -5.075186123 3.30666684]*1e6;
+end
+st=gps2rng({'Unit1-camp.mat','Unit2-camp.mat','Unit3-camp.mat','Unit4-camp.mat'},'ave',xyz,1500,5225);
 
 figure(1)
 clf
@@ -66,21 +80,33 @@ if num == 1
 
     % And replot those new tags
     p(2)=plot(ntags(:,2),'r.','MarkerSize',3);
-    keyboard
+    
     delete(p(1))
     hold off
 elseif num == 2
+    ofx1=1; ofy1=1;
+    ofx2=12500; ofy2=-4;
+    n=1; ofxy1=81000; ofxy2=86000;
+    stoff=0;
+    
     tags(:,2)=unwrap(tags(:,2)/1e9*2*pi)/2/pi;
+    tags=tags(ofx1:end,1:2)+ofy1;
     p(1)=plot(tags(:,2),'g.','MarkerSize',3);
 
-    ntags=retimei(tags,1);
+    ntags=retimei(tags,n);
 
-    p(2)=plot(ntags(:,2),'r.','MarkerSize',3);
-    keyboard
+    ntags(ofxy1+1:end,2)=ntags(ofxy1+1:end,2)+ofy2;
+    ntags=ntags(ofx2:end,1:2);
+    
+    p(2)=plot([1:ofxy2]+stoff,ntags(1:ofxy2,2),'r.','MarkerSize',3);
+
+    delete(p(1))
+    hold off
 elseif num == 3
     ofx1=1; ofy1 = 1;
     ofx2=12000; ofy2=-3;
     n=1; ofxy1=80000;
+    stoff=1000;
     
     % unwrap first
     tags(:,2)=unwrap(tags(:,2)/1e9*2*pi)/2/pi;
@@ -93,24 +119,42 @@ elseif num == 3
     % then retime
     ntags=retimei(tags,n);
 
-    ntags(1:ofxy1,2)=ntags(1:ofxy1,2);
     ntags(ofxy1+1:end,2)=ntags(ofxy1+1:end,2)+ofy2;
     
     ntags=ntags(ofx2:end,1:2);
 
-    p(2)=plot(ntags(:,2),'r.','MarkerSize',3);
-    keyboard
+    p(2)=plot([1:length(ntags)]+stoff,ntags(:,2),'r.','MarkerSize',3);
+
     delete(p(1))
     hold off
-    keyboard
 elseif num == 4
-    % load in DOG4
-end
+    ofx1=2250; ofy1=2; %ofy1 must be integer for some reason
+    ofx2=12000; ofy2=-3;
+    n=1; ofxy1=60000;
+    ofxy2=22000; ofxy3=41000;
+    ofy3=-1;
+    ofxy4=77500; stoff=9000;
     
+    tags(:,2)=unwrap(tags(:,2)/1e9*2*pi)/2/pi;
+    tags=tags(ofx1:end,1:2)+ofy1;
+    p(1)=plot(tags(:,2),'g.','MarkerSize',3);
+
+    ntags=retimei(tags,n);
+    % speed corrections:
+    % end bit between ofxy1 and end
+    ntags(ofxy1+1:end,2)=ntags(ofxy1+1:end,2)+ofy2;
+    % middle bit between ofxy2 and ofxy3
+    ntags(ofxy2:ofxy3,2)=ntags(ofxy2:ofxy3,2)+ofy3;
+    p(2)=plot([1:ofxy4]+stoff,ntags(1:ofxy4,2),'r.','MarkerSize',3);
+    
+    delete(p(1))
+    hold off
+end
+
 % cosmetics
 Nh=24;
 xlim([0 3600*Nh])
-ylim([0.85*min(ntags(:,2)) 1.05*max(ntags(:,2))])
+ylim([0.85*min(st) 1.05*max(st)])
 longticks(gca,2)
 grid on
 xlabel('time [h]')
@@ -126,7 +170,7 @@ hold off
 legs=legend(pl,'GNSS','Acoustic','FontSize',6,'Location','northeast');
 keyboard
 % print pdf
-figdisp('dog2mat2',[],'',2,[],'epstopdf')    
+figdisp(sprintf('unit%i-st',num),[],'',2,[],'epstopdf')    
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 

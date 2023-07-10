268a0e5719a8f79271e7be4323d1567117dded16
Frederik J. Simons <fjsimons@gmail.com>
2023-07-10 17:18:07 -0400

    Turns a depth, elevation and sound speed to effective sound velocity

---
+++ dec2esv.m
@@ -0,0 +1,3 @@
+% Converts depth and elevation and sounds speed profile into a travel time and a
+% distance and an effective sound velocity. Makes a table, and/or reads a table,
+% and/or interpolates.
